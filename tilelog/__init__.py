c4c7a99dba6f405bb30b3c98afcc66d386acde95
Paul Norman <penorman@mac.com>
2022-07-20 16:37:39 -0700

    Produce logs of app usage

--- tilelog/__init__.py
+++ tilelog/__init__.py
@@ -25,7 +25,9 @@ MIN_TPS = 5.0
               help="File to output tile usage logs to")
 @click.option('--host', type=click.File('w', encoding='utf-8'),
               help="File to output host usage logs to")
-def cli(date, staging, region, tile, host):
+@click.option('--app', type=click.File('w', encoding='utf-8'),
+              help="File to output app usage logs to")
+def cli(date, staging, region, tile, host, app):
     click.echo("Generating files for {}".format(date.strftime("%Y-%m-%d")))
     with pyathena.connect(s3_staging_dir=staging,
                           region_name=region).cursor() as curs:
@@ -33,6 +35,8 @@ def cli(date, staging, region, tile, host):
             tile_logs(curs, date, tile)
         if host is not None:
             host_logs(curs, date, host)
+        if app is not None:
+            app_logs(curs, date, app)
 
 
 def tile_logs(curs, date, dest):
@@ -118,3 +122,58 @@ ORDER BY COUNT(*) DESC;
     csvwriter = csv.writer(dest, dialect=csv.unix_dialect,
                            quoting=csv.QUOTE_NONNUMERIC)
     csvwriter.writerows(sorted_hosts)
+
+def app_logs(curs, date, dest):
+    click.echo("Querying for app usage")
+    query = """
+SELECT
+app,
+cast(count(*) as double)/86400 AS tps,
+cast(count(*) filter (WHERE cachehit = 'MISS') as double)/86400 AS tps_miss
+FROM (
+    SELECT
+    CASE
+    WHEN requestedwith != '' THEN requestedwith
+    WHEN useragent LIKE 'Mozilla/5.0 QGIS/%%' THEN 'Mozilla/5.0 QGIS/*'
+    WHEN useragent LIKE 'qfield|%%|%%|%%| QGIS/%%' THEN 'qfield|*|*|*| QGIS/*'
+    WHEN useragent LIKE 'Mozilla/5.0 (compatible; Marble/%%)' THEN 'Mozilla/5.0 (compatible; Marble/*)'
+    WHEN useragent LIKE 'JOSM/1.5 %%' THEN 'JOSM/1.5 *'
+    WHEN useragent LIKE 'OruxMaps %%' THEN 'OruxMaps *'
+    WHEN useragent LIKE 'MapProxy-%%' THEN 'MapProxy-*'
+    WHEN useragent LIKE 'Dart/%% (dart:io)' THEN 'Dart/* (dart:io)'
+    WHEN useragent LIKE 'Wikiloc %%' THEN 'Wikiloc *'
+    WHEN useragent LIKE 'Mapbox iOS SDK (%%)' THEN 'Mapbox iOS SDK (*)'
+
+    -- Extract app name from foo.bar/123.456
+    WHEN regexp_like(useragent, '^([^./]+(\.[^./]+)*)/\d+(\.\d+)*$') THEN regexp_extract(useragent, '^([^./]+(\.[^./]+)*)/\d+(\.\d+)*$', 1) || '/*'
+    -- Some apps have extra stuff after the name/version
+    WHEN regexp_like(useragent, '^([^/ ]+)(/[^/ ]+) CFNetwork/[^/ ]+ Darwin/[^/ ]+$') THEN regexp_extract(useragent, '^([^/ ]+)(/[^/ ]+) CFNetwork/[^/ ]+ Darwin/[^/ ]+$', 1) || '/* CFNetwork/* Darwin/*'
+    -- Mapbox apps follow the pattern Bikemap/22.0.2 Mapbox/5.13.0-pre.1 MapboxGL/0.0.0 (c6fb3581) iOS/15.5.0 (arm64)
+    WHEN regexp_like(useragent, '^([^/ ]+)(/[^/ ]+) Mapbox/') THEN regexp_extract(useragent, '^([^/ ]+)(/[^/ ]+) Mapbox/', 1) || '/* Mapbox/*'
+    WHEN regexp_like(useragent, '^([^/ ]+)(/[^/ ]+) GLMap/') THEN regexp_extract(useragent, '^([^/ ]+)(/[^/ ]+) GLMap/', 1) || '/* GLMap/*'
+
+    WHEN useragent LIKE 'Mozilla/%%' AND referer != '' THEN referer -- This will only show referers that are not https ones, e.g. flash apps
+    ELSE useragent END AS app,
+    cachehit
+    FROM logs.fastly_logs_v18
+WHERE status IN (200, 206, 304)
+    AND year = %(year)d
+    AND month = %(month)d
+    AND day = %(day)d
+    AND (
+        requestedwith != '' -- Leaflet-based apps
+        OR referer = '' -- Non-websites
+        OR (referer NOT LIKE 'https://%%/%%' AND referer NOT LIKE 'http://%%/%%') -- Referer set, but not a website
+    )
+) AS combined_requests
+GROUP BY app
+HAVING COUNT(*) > %(tps)d*86400
+ORDER BY COUNT(*) DESC
+    """
+
+    curs.execute(query, {"year": date.year, "month": date.month,
+                         "day": date.day, "tps": MIN_TPS})
+    click.echo("Writing host usage to file")
+    csvwriter = csv.writer(dest, dialect=csv.unix_dialect,
+                           quoting=csv.QUOTE_NONNUMERIC)
+    csvwriter.writerows(curs)
