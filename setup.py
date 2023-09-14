e992535b0ecb60e7c8755ee2276c5321346453e3
Paul Norman <penorman@mac.com>
2023-09-13 20:44:53 -0700

    Update to v1.7.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='tilelog',
-    version='1.6.1',
+    version='1.7.0',
     author="Paul Norman",
     author_email="osm@paulnorman.ca",
     url="https://github.com/openstreetmap/tilelog",
